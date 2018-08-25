8f470c25aae6ea78020b685fca898990b4a1516f
steklopod <steklopod@gmail.com>
2018-08-25 15:43:10 +0300

    Companion object [first example]- v0.3

--- src/main/scala/companion/Companion.scala
+++ src/main/scala/companion/Companion.scala
@@ -2,5 +2,25 @@ package companion
 
 object Companion {
 
+  class Person(val firstName: String, val middleName: String, val lastName: String) {
+    override def toString = s"Person($firstName, $middleName, $lastName)"
+  }
+  object Person {
+    def apply(firstName: String, lastName: String) = new Person(firstName, "", lastName)
+    def apply(firstName: String)                   = new Person(firstName, "", "")
+  }
 
 }
+
+object PersonTest extends App{
+  import companion.Companion.Person
+
+  val p = Person("Дима", "Колтович")
+  println(p)
+}
+
+//2 пример//////////////////////////
+
+
+
+
