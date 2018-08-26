f8cceb4b1f1344a0bad35f088d5569d7c46e81af
steklopod <steklopod@gmail.com>
2018-08-26 17:32:03 +0300

    Companion object - v0.5

--- src/main/scala/companion/Companion.scala
+++ src/main/scala/companion/Companion.scala
@@ -21,6 +21,34 @@ object PersonTest extends App{
 
 //2 пример//////////////////////////
 
+abstract class AnimalCounter {
+  var animals = 0
+  def name: String
+  def count() {
+    animals += 1
+    println("%d %ss created so far".format(animals, name))
+  }
+}
 
+abstract class Animal {
+  def companion: AnimalCounter
+  companion.count()
+}
+
+class  Dog extends Animal { def companion = Dog }
+object Dog extends AnimalCounter {
+  val name = "dog"
+}
+
+class  Cat extends Animal { def companion = Cat }
+object Cat extends AnimalCounter {
+  val name = "cat"
+}
+
+object AnimalTest extends App{
+  new Dog
+  new Cat
+  new Cat
+}
 
 
