71e6c70548389c915a803fd3e23ea61324381aef
Dima Koltovich <steklopod@gmail.com>
2018-08-28 15:50:02 +0300

    implicit parameters example added

---
+++ src/main/scala/OOP/implicits/Implicit_01.scala
@@ -0,0 +1,29 @@
+package OOP.implicits
+
+
+object Implicit_01 extends App {
+  implicit def strToMyString(x: String): MyString = MyString(x)
+
+  case class MyString(s: String) {
+    def whose = s"I'm yours :] $s"
+  }
+
+  println("heh".whose)
+}
+
+//2///////////
+
+object Squaring {
+  implicit class Squarer(x: Int) {
+    def numberSquaring[A](f: => A): Unit = {
+      println(x + " * " + x + " = " + x * x);
+    }
+  }
+}
+
+object SquaringDemo extends App {
+  import OOP.implicits.Squaring._
+
+    100 numberSquaring()
+}
+
---
+++ src/main/scala/OOP/implicits/Monoid.scala
@@ -0,0 +1,30 @@
+package OOP.implicits
+
+abstract class Monoid[A] {
+  def add(x: A, y: A): A
+  def unit: A
+}
+
+object ImplicitEx {
+  implicit val stringMonoid: Monoid[String] = new Monoid[String] {
+    def add(x: String, y: String): String = x concat y
+    def unit: String = ""
+  }
+
+  implicit val intMonoid: Monoid[Int] = new Monoid[Int] {
+    def add(x: Int, y: Int): Int = x + y
+    def unit: Int = 0
+  }
+
+  def sum[A](xs: List[A])(implicit m: Monoid[A]): A =
+    if (xs.isEmpty) m.unit
+    else m.add(xs.head, sum(xs.tail))
+}
+
+object ImplicitExTest extends App{
+  import OOP.implicits.ImplicitEx._
+
+  println(sum(List(1, 2, 3)))       // использует IntMonoid неявно
+  println(sum(List("a", "b", "c"))) // использует StringMonoid неявно
+}
+
