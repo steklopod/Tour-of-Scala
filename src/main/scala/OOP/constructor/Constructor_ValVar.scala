7551c23afe4e55e2b73f151172d4c4da86b5bc52
Dima Koltovich <steklopod@gmail.com>
2018-11-01 13:21:23 +0300

    Multiple Constructor examples added

--- src/main/scala/OOP/constructor/Constructor_ValVar.scala
+++ src/main/scala/OOP/constructor/Constructor_ValVar.scala
@@ -15,8 +15,8 @@ object ClassConstructorTest extends App {
   val x = new Constructor_ValVar(1, 2, 3)
 
   // Нельзя вызвать x.a
-  println(x.b) // 2
-  println(x.c) // 3
+  println(x.b)    // 2
+  println(x.c)    // 3
   println(x.summ) // 6
 
   x.c = 97
@@ -24,13 +24,12 @@ object ClassConstructorTest extends App {
   println(x.summ) // 100
 }
 
-
-//2 пример//////////////////////////
+//2 пример
 
 class Complex(val real: Double, val image: Double) extends Ordered[Complex] {
   def magnitude            = Math.sqrt(real * real + image * image)
   def angle                = Math.atan2(image, real)
-  def +(that: Complex)            = new Complex(this.real + that.real, this.image + that.image)
+  def +(that: Complex)    = new Complex(this.real + that.real, this.image + that.image)
   def compare(that: Complex): Int = this.magnitude compare that.magnitude
 
   override def toString = real + " + i*" + image + " | " + magnitude + "*e^(i*" + angle + "))"
@@ -40,14 +39,13 @@ object Main extends App {
   val first  = new Complex(1, 5)
   val second = new Complex(2, 4)
 
-  if (first > second)   println("Первый больше")
-  if (first < second)   println("Второй больше")
-  if (first == second)  println("Объекты эквивалентны")
+  if (first > second)  println("Первый больше")
+  if (first < second)  println("Второй больше")
+  if (first == second) println("Объекты эквивалентны")
 }
-
 
 class User {
   private[this] var _name: String = ""
-  def name                  = _name toUpperCase
-  def name_=(name: String)   = _name =  if (name != null) name else ""
+  def name               = _name toUpperCase
+  def name_=(name: String)= _name = if (name != null) name else ""
 }
--- src/main/scala/OOP/constructor/MultipleConstructor.scala
+++ src/main/scala/OOP/constructor/MultipleConstructor.scala
@@ -4,12 +4,12 @@ class Person(val имя: String, val фамилия: String, val возраст:
 
   def this(имя: String) {
     this(имя, "", 0)
-      println("\n Фамилия и возраст неизвестны.")
+    println("\n Фамилия и возраст неизвестны.")
   }
 
   def this(имя: String, фамилия: String) {
     this(имя, фамилия, 0)
-      println("\n Возраст неизвестен.")
+    println("\n Возраст неизвестен.")
   }
 
   override def toString: String = "%s %s, возраст: %d".format(имя, фамилия, возраст)
@@ -17,14 +17,12 @@ class Person(val имя: String, val фамилия: String, val возраст:
 
 object MultipleConstructor extends App {
 
-    val всеАргументы = new Person("Дима", "Колтович", 20)
-      println(всеАргументы)
+  val всеАргументы = new Person("Дима", "Колтович", 20)
+  println(всеАргументы)
 
-    val коля = new Person("Коля", "Воронкович")
-      println(коля)
+  val коля = new Person("Коля", "Воронкович")
+  println(коля)
 
-    val вася = new Person("Вася")
-      println(вася)
+  val вася = new Person("Вася")
+  println(вася)
 }
-
-
